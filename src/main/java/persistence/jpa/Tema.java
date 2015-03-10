3a3a815d216809abb1ca2b2cb0f1a449505d2670
nataliaznar <nataliaznar@hotmail.com>
2015-03-10 21:03:40 +0100

    Final

--- src/main/java/persistence/jpa/Tema.java
+++ src/main/java/persistence/jpa/Tema.java
@@ -1,5 +1,16 @@
 package persistence.jpa;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+
+@Entity
 public class Tema {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    
+    private String nombre;
+    private String pregunta;
 
 }
