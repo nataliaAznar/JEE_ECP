01182307010926349ee9c858e69040894f513543
nataliaAznar <natalia.aznar@opendeusto.es>
2015-03-13 20:29:34 +0100

    Update class DaoJpaFactory

--- src/main/java/persistence/models/daos/jpa/DaoJpaFactory.java
+++ src/main/java/persistence/models/daos/jpa/DaoJpaFactory.java
@@ -6,6 +6,8 @@ import javax.persistence.Persistence;
 import org.apache.logging.log4j.LogManager;
 
 import persistence.models.daos.DaoFactory;
+import persistence.models.daos.TemaDao;
+import persistence.models.daos.VotoDao;
 
 public class DaoJpaFactory extends DaoFactory {
     private static final String PERSISTENCE_UNIT = "tictactoe";
@@ -20,6 +22,16 @@ public class DaoJpaFactory extends DaoFactory {
     public static EntityManagerFactory getEntityManagerFactory() {
         return entityManagerFactory;
     }
+
+	@Override
+	public TemaDao getTemaDao() {
+		return new TemaDaoJpa();
+	}
+
+	@Override
+	public VotoDao getVotoDao() {
+		return new VotoDaoJpa();
+	}
 
 
 }
