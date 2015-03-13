315bf08036e46aa6639aa7af021bd9fcf84e16ab
nataliaAznar <natalia.aznar@opendeusto.es>
2015-03-13 15:47:40 +0100

    drop and create copied from JEE.Persistencia

--- src/main/java/persistence/jpa/CreateTables.java
+++ src/main/java/persistence/jpa/CreateTables.java
@@ -1,5 +1,19 @@
 package persistence.jpa;
 
-public class CreateTables {
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+import org.eclipse.persistence.config.PersistenceUnitProperties;
+
+
+public class CreateTables {
+	 public static void main(String[] args) {
+	        //Se borrar y se crean las tablas
+	        Map<String, String> properties = new HashMap<>();
+	        properties.put(PersistenceUnitProperties.DDL_GENERATION,
+	                PersistenceUnitProperties.DROP_AND_CREATE);
+	    }
 }
