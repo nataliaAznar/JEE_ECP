66149fcfde0473269febccf3f8ba3e402cfcb331
nataliaAznar <natalia.aznar@opendeusto.es>
2015-03-13 20:32:50 +0100

    Update class TemaDaoJpaTest Final

--- src/test/java/persistence/models/daos/jpa/TemaDaoJpaTest.java
+++ src/test/java/persistence/models/daos/jpa/TemaDaoJpaTest.java
@@ -1,6 +1,16 @@
 package persistence.models.daos.jpa;
 
+import org.junit.Before;
+
+import persistence.models.daos.TemaDao;
+
 public class TemaDaoJpaTest {
 
+	private TemaDao dao = DaoJpaFactory.getFactory().getTemaDao();
+	
+	@Before
+	public void init(){
+		
+	}
 	
 }
