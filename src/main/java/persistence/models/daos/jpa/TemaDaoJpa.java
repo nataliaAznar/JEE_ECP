73d5623ff39364446127962a408c9988e7e6a0fd
nataliaAznar <natalia.aznar@opendeusto.es>
2015-03-13 18:00:09 +0100

    Add class TemaDaoJpa

---
+++ src/main/java/persistence/models/daos/jpa/TemaDaoJpa.java
@@ -0,0 +1,13 @@
+package persistence.models.daos.jpa;
+
+import persistence.jpa.Tema;
+import persistence.models.daos.TemaDao;
+
+
+public class TemaDaoJpa extends GenericDaoJpa<Tema, Integer> implements TemaDao {
+
+	public TemaDaoJpa() {
+		super(Tema.class);
+	}
+
+}
