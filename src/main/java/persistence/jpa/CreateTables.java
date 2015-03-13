e32ec83b47007b72212faf513cca2e65e5868d3b
nataliaAznar <natalia.aznar@opendeusto.es>
2015-03-13 17:43:26 +0100

    Updated CreateTables Uses JpaFactory

--- src/main/java/persistence/jpa/CreateTables.java
+++ src/main/java/persistence/jpa/CreateTables.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import org.eclipse.persistence.config.PersistenceUnitProperties;
@@ -11,11 +12,9 @@ import org.eclipse.persistence.config.PersistenceUnitProperties;
 
 public class CreateTables {
 	 public static void main(String[] args) {
-	        //Se borrar y se crean las tablas
-	        Map<String, String> properties = new HashMap<>();
-	        properties.put(PersistenceUnitProperties.DDL_GENERATION,
-	                PersistenceUnitProperties.DROP_AND_CREATE);
-	        EntityManager em = Persistence.createEntityManagerFactory("miwjee", properties)
-	                .createEntityManager();
+	        //Se borrar y se crean las tabla        
+		 JpaFactory.dropAndCreateTables();
+		 EntityManagerFactory em = JpaFactory.getEntityManagerFactory();
+		 em.createEntityManager();
 	    }
 }
