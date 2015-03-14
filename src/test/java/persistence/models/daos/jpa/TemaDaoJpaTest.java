891d448b29b92971b12d4561c53cb9f2ce273d32
nataliaznar <nataliaznar@hotmail.com>
2015-03-14 10:41:38 +0100

    Update class TemaDaoJpaTest

--- src/test/java/persistence/models/daos/jpa/TemaDaoJpaTest.java
+++ src/test/java/persistence/models/daos/jpa/TemaDaoJpaTest.java
@@ -1,15 +1,27 @@
 package persistence.models.daos.jpa;
 
-import org.junit.Before;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import persistence.jpa.Tema;
 import persistence.models.daos.TemaDao;
 
 public class TemaDaoJpaTest {
 
 	private TemaDao dao = DaoJpaFactory.getFactory().getTemaDao();
+	private List<Tema> temas = new ArrayList();
 	
 	@Before
 	public void init(){
+		Tema t = new Tema();
+	}
+	
+	@Test
+	public void testCreate(){
+		
 		
 	}
 	
