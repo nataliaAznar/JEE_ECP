7f73e60919340c6cd8b368bedeefa07c71e24fe2
nataliaznar <nataliaznar@hotmail.com>
2015-03-14 10:46:18 +0100

    Update class Voto

--- src/main/java/persistence/jpa/Voto.java
+++ src/main/java/persistence/jpa/Voto.java
@@ -18,4 +18,52 @@ public class Voto {
 	 @ManyToOne
 	 @JoinColumn
 	 private Tema tema;
+	 
+	public Voto() {
+	}
+	
+	public Voto(Integer puntuacion, Integer estudios, String ip, Tema tema) {
+		this.puntuacion = puntuacion;
+		this.estudios = estudios;
+		this.ip = ip;
+		this.tema = tema;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public Integer getPuntuacion() {
+		return puntuacion;
+	}
+
+	public void setPuntuacion(Integer puntuacion) {
+		this.puntuacion = puntuacion;
+	}
+
+	public Integer getEstudios() {
+		return estudios;
+	}
+
+	public void setEstudios(Integer estudios) {
+		this.estudios = estudios;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+	 
+	 
 }
