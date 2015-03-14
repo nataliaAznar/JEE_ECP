fb5daadc07aa1a9d4e19694a6aa320fdbfa58a0a
nataliaznar <nataliaznar@hotmail.com>
2015-03-14 10:44:13 +0100

    Update class Tema

--- src/main/java/persistence/jpa/Tema.java
+++ src/main/java/persistence/jpa/Tema.java
@@ -12,4 +12,34 @@ public class Tema {
     
     private String nombre;
     private String pregunta;
+    
+	public Tema() {
+	}
+
+	public Tema(String nombre, String pregunta) {
+		super();
+		this.nombre = nombre;
+		this.pregunta = pregunta;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getPregunta() {
+		return pregunta;
+	}
+	
+	public void setPregunta(String pregunta) {
+		this.pregunta = pregunta;
+	}
+	
 }
