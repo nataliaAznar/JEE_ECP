2a4e0e0201d8b74ab8d745e82a59540a386231e4
nataliaznar <nataliaznar@hotmail.com>
2015-03-19 12:57:32 +0100

    Update class VotacionesController

--- src/main/java/controllers/VotacionesController.java
+++ src/main/java/controllers/VotacionesController.java
@@ -1,5 +1,22 @@
 package controllers;
 
+import java.util.List;
+
+import persistence.jpa.Tema;
+import persistence.models.daos.DaoFactory;
+import persistence.models.daos.TemaDao;
+import persistence.models.daos.jpa.DaoJpaFactory;
+
 public class VotacionesController {
+	private TemaDao daoTema;
+	
+	public VotacionesController() {
+		DaoFactory.setFactory(new DaoJpaFactory());
+		daoTema = DaoJpaFactory.getFactory().getTemaDao();
+	}
+
+	public List<Tema> getTemas(){
+		return daoTema.findAll();
+	}
 
 }
