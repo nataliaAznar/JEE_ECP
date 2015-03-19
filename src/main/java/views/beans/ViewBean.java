be44ecb573a263b0c6a2eb2b5dcaed7533e41217
nataliaznar <nataliaznar@hotmail.com>
2015-03-19 13:05:27 +0100

    Add class ViewsBean copied form TicTacToe

---
+++ src/main/java/views/beans/ViewBean.java
@@ -0,0 +1,20 @@
+package views.beans;
+
+import javax.faces.bean.ManagedProperty;
+
+import controllers.ControllerFactory;
+
+public abstract class ViewBean {
+
+    @ManagedProperty(value = "#{controllerFactory}")
+    private ControllerFactory controllerFactory;
+
+    public void setControllerFactory(ControllerFactory controllerFactory) {
+        this.controllerFactory = controllerFactory;
+    }
+
+    protected ControllerFactory getControllerFactory() {
+        return controllerFactory;
+    }
+
+}
