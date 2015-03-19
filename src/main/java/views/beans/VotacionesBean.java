c323c63251a7b0142fca6e16a7a1de66ef42841f
nataliaznar <nataliaznar@hotmail.com>
2015-03-19 13:08:23 +0100

    Update class VotacionesBean

--- src/main/java/views/beans/VotacionesBean.java
+++ src/main/java/views/beans/VotacionesBean.java
@@ -1,22 +1,22 @@
 package views.beans;
 
 import java.io.Serializable;
+import java.util.List;
 
-import org.apache.logging.log4j.LogManager;
-
+import controllers.VotacionesController;
 import persistence.jpa.Tema;
 
-public class VotacionesBean implements Serializable{
+public class VotacionesBean extends ViewBean implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
-	private Tema[] temas;
+	private List<Tema> temas;
 	
 	public VotacionesBean() {
 	}
 
 	public void update() {
-		this.temas = this.getControllerFactory()
-        
+		VotacionesController votacionesController = this.getControllerFactory().getVotacionesController();
+		this.temas = votacionesController.getTemas();
     }
 
 	
