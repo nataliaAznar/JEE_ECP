a2936b46f051b6fc2260652f103ca1dd2233539a
nataliaznar <nataliaznar@hotmail.com>
2015-03-19 12:46:19 +0100

    Update classes Votaciones Bean and votaciones

--- src/main/java/views/beans/VotacionesBean.java
+++ src/main/java/views/beans/VotacionesBean.java
@@ -1,5 +1,64 @@
 package views.beans;
 
-public class VotacionesBean {
+import java.io.Serializable;
 
+public class VotacionesBean implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private int puntuacion;
+	private int estudios;
+	private String ip;
+	private String tema;
+	private int temaId;
+	
+	public VotacionesBean() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getPuntuacion() {
+		return puntuacion;
+	}
+
+	public void setPuntuacion(int puntuacion) {
+		this.puntuacion = puntuacion;
+	}
+
+	public int getEstudios() {
+		return estudios;
+	}
+
+	public void setEstudios(int estudios) {
+		this.estudios = estudios;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public String getTema() {
+		return tema;
+	}
+
+	public void setTema(String tema) {
+		this.tema = tema;
+	}
+
+	public int getTemaId() {
+		return temaId;
+	}
+
+	public void setTemaId(int temaId) {
+		this.temaId = temaId;
+	}
+
+
+	
 }
