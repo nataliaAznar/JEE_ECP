9d8560c4b6b10825d2601fd992cd9528672c6702
nataliaznar <nataliaznar@hotmail.com>
2015-03-21 12:42:40 +0100

    Update class VotarBean

--- src/main/java/views/beans/jsf/VotarBean.java
+++ src/main/java/views/beans/jsf/VotarBean.java
@@ -1,5 +1,43 @@
 package views.beans.jsf;
 
+import javax.faces.bean.ManagedBean;
+
+import persistence.jpa.Tema;
+
+@ManagedBean
 public class VotarBean {
+	private Tema tema;
+	
+	private static String[] estudios = {"ESO", "Bachiller", "Universitario"};
+	private static int[] puntuacion = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	
+	public VotarBean() {
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
+	public static String[] getEstudios() {
+		return estudios;
+	}
+
+	public static void setEstudios(String[] estudios) {
+		VotarBean.estudios = estudios;
+	}
+
+	public static int[] getPuntuacion() {
+		return puntuacion;
+	}
+
+	public static void setPuntuacion(int[] puntuacion) {
+		VotarBean.puntuacion = puntuacion;
+	}
+	
+	
 
 }
