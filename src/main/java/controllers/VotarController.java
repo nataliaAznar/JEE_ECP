a98e2e3e1ecfee9c0e5e9be833436a11dabdbf13
nataliaznar <nataliaznar@hotmail.com>
2015-03-22 20:18:16 +0100

    Update class VotarController

--- src/main/java/controllers/VotarController.java
+++ src/main/java/controllers/VotarController.java
@@ -1,5 +1,22 @@
 package controllers;
 
+import java.util.List;
+
+import persistence.jpa.Tema;
+import persistence.models.daos.DaoFactory;
+import persistence.models.daos.TemaDao;
+import persistence.models.daos.jpa.DaoJpaFactory;
+
 public class VotarController {
+	private TemaDao daoTema;
+
+	public VotarController() {
+		DaoFactory.setFactory(new DaoJpaFactory());
+		this.daoTema = DaoJpaFactory.getFactory().getTemaDao();
+	}
+	
+	public List<Tema> getTemas(){
+		return daoTema.findAll();
+	}
 
 }
