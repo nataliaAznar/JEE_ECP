c8e6e97b40ef2d0ca5aab079d4eabf65cb7a5520
nataliaznar <nataliaznar@hotmail.com>
2015-03-24 14:54:37 +0100

    Update class VotarController

--- src/main/java/controllers/VotarController.java
+++ src/main/java/controllers/VotarController.java
@@ -3,12 +3,15 @@ package controllers;
 import java.util.List;
 
 import persistence.jpa.Tema;
+import persistence.jpa.Voto;
 import persistence.models.daos.DaoFactory;
 import persistence.models.daos.TemaDao;
+import persistence.models.daos.VotoDao;
 import persistence.models.daos.jpa.DaoJpaFactory;
 
 public class VotarController {
 	private TemaDao daoTema;
+	private VotoDao daoVoto;
 
 	public VotarController() {
 		DaoFactory.setFactory(new DaoJpaFactory());
@@ -20,7 +23,9 @@ public class VotarController {
 	}
 	
 	public void addVoto(int idTema, int estudios, int puntuacion, String ip){
-		
+		Tema tema = daoTema.read(idTema);
+		Voto voto = new Voto(puntuacion, estudios, ip, tema);
+		daoVoto.create(voto);
 	}
 
 }
