28c63779a2ffe19a7a1dd194cbb66c9d49930c74
nataliaznar <nataliaznar@hotmail.com>
2015-03-24 12:22:08 +0100

    Update class VotarBean

--- src/main/java/views/beans/jsf/VotarBean.java
+++ src/main/java/views/beans/jsf/VotarBean.java
@@ -3,24 +3,32 @@ package views.beans.jsf;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import controllers.VotarController;
 import persistence.jpa.Tema;
 import views.beans.jsp.ViewBean;
 
 @ManagedBean
+@ViewScoped
 public class VotarBean extends ViewBean implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private List<Tema> temas;
+	private int idTema;
 	
 	private String[] estudios = {"ESO", "Bachiller", "Universitario"};
 	private int[] puntuacion = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	
 	private int length = estudios.length;
+	private String pregunta;
 	
 	private String estudio;
 	private int puntos;
+	
+	private boolean disabledVote;
+
 	
 	public VotarBean() {
 	}
@@ -69,10 +77,41 @@ public class VotarBean extends ViewBean implements Serializable{
 	public void setTemas(List<Tema> temas) {
 		this.temas = temas;
 	}
+	
+	public int getIdTema() {
+		return idTema;
+	}
 
-	public void update(){
+	public void setIdTema(int idTema) {
+		this.idTema = idTema;
+		this.updateVote();
+	}
+
+	public String getPregunta() {
+		return pregunta;
+	}
+
+	public void setPregunta(String pregunta) {
+		this.pregunta = pregunta;
+	}
+
+	public boolean isDisabledVote() {
+		return disabledVote;
+	}
+
+	@PostConstruct
+    public void update() {
 		VotarController votarController = this.getControllerFactory().getVotarController();
 		this.temas = votarController.getTemas();
-	}
+        this.temas.add(0, new Tema(-1, "Elige", ""));
+        this.idTema = -1;
+        this.updateVote();
+    }
+
+	private void updateVote() {
+        pregunta = temas.get((idTema+1)).getPregunta();
+        puntos = puntuacion[0];
+        disabledVote = idTema == -1;
+    }
 
 }
