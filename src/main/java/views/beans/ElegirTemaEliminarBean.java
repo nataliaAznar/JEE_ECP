969103d738e9957df95fd82628ca4873b0c84cd6
nataliaznar <nataliaznar@hotmail.com>
2015-03-25 16:12:55 +0100

    Update class ElegirTemaEliminarBean

--- src/main/java/views/beans/ElegirTemaEliminarBean.java
+++ src/main/java/views/beans/ElegirTemaEliminarBean.java
@@ -1,5 +1,50 @@
 package views.beans;
 
-public class ElegirTemaEliminarBean {
+import java.io.Serializable;
+import java.util.List;
+
+import persistence.jpa.Tema;
+import controllers.EliminarTemaController;
+
+public class ElegirTemaEliminarBean extends ViewBean implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private EliminarTemaController eliminarTemaController = new EliminarTemaController();
+	
+	private List<Tema> temas;
+	private int idTema;
+	
+	public ElegirTemaEliminarBean() {
+	}
+
+	public EliminarTemaController getEliminarTemaController() {
+		return eliminarTemaController;
+	}
+
+	public void setEliminarTemaController(
+			EliminarTemaController eliminarTemaController) {
+		this.eliminarTemaController = eliminarTemaController;
+	}
+
+	public List<Tema> getTemas() {
+		return temas;
+	}
+
+	public void setTemas(List<Tema> temas) {
+		this.temas = temas;
+	}
+
+	public int getIdTema() {
+		return idTema;
+	}
+
+	public void setIdTema(int idTema) {
+		this.idTema = idTema;
+	}
+	
+	public void update() {
+		this.temas = eliminarTemaController.getTemas();
+        this.temas.add(0, new Tema(-1, "Elige", ""));
+        this.idTema = -1;
+    }
 
 }
