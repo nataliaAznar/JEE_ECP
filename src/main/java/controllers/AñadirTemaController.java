5e0bfa6ef905136be369432245376d9ec40fbfde
nataliaznar <nataliaznar@hotmail.com>
2015-03-25 12:11:51 +0100

    Update class AñadirTemaController

--- src/main/java/controllers/AñadirTemaController.java
+++ src/main/java/controllers/AñadirTemaController.java
@@ -1,5 +1,21 @@
 package controllers;
 
+import persistence.jpa.Tema;
+import persistence.models.daos.DaoFactory;
+import persistence.models.daos.TemaDao;
+import persistence.models.daos.jpa.DaoJpaFactory;
+
 public class AñadirTemaController {
+	private TemaDao daoTema;
+
+	public AñadirTemaController() {
+		DaoFactory.setFactory(new DaoJpaFactory());
+		this.daoTema = DaoJpaFactory.getFactory().getTemaDao();
+	}
+	
+	public void addTema(String nombre, String pregunta){
+		Tema tema = new Tema(nombre, pregunta);
+		daoTema.create(tema);
+	}
 
 }
