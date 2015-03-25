67d6787bf4215c650c76a9f1888369bf523cd8c0
nataliaznar <nataliaznar@hotmail.com>
2015-03-25 11:15:32 +0100

    Update class AñadirTemaBean

--- src/main/java/views/beans/AñadirTemaBean.java
+++ src/main/java/views/beans/AñadirTemaBean.java
@@ -1,5 +1,30 @@
 package views.beans;
 
-public class AñadirTemaBean {
+import java.io.Serializable;
+
+public class AñadirTemaBean extends ViewBean implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String nombre;
+	private String pregunta;
+	
+	public AñadirTemaBean() {
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPregunta() {
+		return pregunta;
+	}
+
+	public void setPregunta(String pregunta) {
+		this.pregunta = pregunta;
+	}
 
 }
