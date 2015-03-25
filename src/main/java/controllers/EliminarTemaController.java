384eb6b9122cecd591bfb9168b17676ef83d96a8
nataliaznar <nataliaznar@hotmail.com>
2015-03-25 16:03:35 +0100

    Update class EliminarTemaController

--- src/main/java/controllers/EliminarTemaController.java
+++ src/main/java/controllers/EliminarTemaController.java
@@ -1,5 +1,27 @@
 package controllers;
 
+import java.util.List;
+
+import persistence.jpa.Tema;
+import persistence.models.daos.DaoFactory;
+import persistence.models.daos.TemaDao;
+import persistence.models.daos.jpa.DaoJpaFactory;
+
 public class EliminarTemaController {
+	private TemaDao daoTema;
+
+	public EliminarTemaController() {
+		DaoFactory.setFactory(new DaoJpaFactory());
+		this.daoTema = DaoJpaFactory.getFactory().getTemaDao();
+	}
+
+	public List<Tema> getTemas(){
+		return daoTema.findAll();
+	}
+	
+	public void deleteTema(int id){
+		daoTema.deleteById(id);
+	}
+	
 
 }
