b662d713a8c2e3e65c8346dfe24a9104723c6a10
nataliaznar <nataliaznar@hotmail.com>
2015-03-25 14:27:26 +0100

    Update class EliminarTemaBean

--- src/main/java/views/beans/EliminarTemaBean.java
+++ src/main/java/views/beans/EliminarTemaBean.java
@@ -1,5 +1,38 @@
 package views.beans;
 
-public class EliminarTemaBean {
+import java.io.Serializable;
+
+public class EliminarTemaBean extends ViewBean implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private static int identificador = 666;
+	
+	private String error = "Identificador inválido";
+	private boolean showError = false;
+	private int identificadorIntroducido;
+	
+	public EliminarTemaBean() {
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public boolean isShowError() {
+		return showError;
+	}
+
+	public void setShowError(boolean showError) {
+		this.showError = showError;
+	}
+
+	public int getIdentificadorIntroducido() {
+		return identificadorIntroducido;
+	}
+
+	public void setIdentificadorIntroducido(int identificadorIntroducido) {
+		this.identificadorIntroducido = identificadorIntroducido;
+	}
+	
+	
 
 }
