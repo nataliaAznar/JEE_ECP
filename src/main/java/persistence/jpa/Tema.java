005b91705b93f1ec2c56fea6dcca372fbea6b5bc
nataliaznar <nataliaznar@hotmail.com>
2015-03-25 12:24:49 +0100

    Update class Tema

--- src/main/java/persistence/jpa/Tema.java
+++ src/main/java/persistence/jpa/Tema.java
@@ -26,6 +26,13 @@ public class Tema {
 		this.nombre = nombre;
 		this.pregunta = pregunta;
 	}
+	
+	
+	public Tema(Integer id, String nombre, String pregunta) {
+		this.id = id;
+		this.nombre = nombre;
+		this.pregunta = pregunta;
+	}
 
 	public Integer getId() {
 		return id;
