b577dfc3cc421b2a4af83c891cb0d9994131dc76
nataliaznar <nataliaznar@hotmail.com>
2015-03-26 18:25:54 +0100

    Update class HomeBean

--- src/main/java/views/beans/HomeBean.java
+++ src/main/java/views/beans/HomeBean.java
@@ -1,24 +1,15 @@
 package views.beans;
 
 import java.io.Serializable;
-import java.util.Locale;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.context.FacesContext;
 
 @ManagedBean
-public class HomeBean implements Serializable{
+public class HomeBean extends ViewBean implements Serializable{
 	private static final long serialVersionUID = 1L;
-	private Locale locale;
 	
 	public HomeBean() {
-		this.locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
+		super();
 	}
-
-	public String change(String language) {
-        this.locale = new Locale(language);
-        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
-        return null;
-    }
 
 }
