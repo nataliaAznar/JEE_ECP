4d4c8b65b287b1363e86e75764f497f1f2d134c9
nataliaznar <nataliaznar@hotmail.com>
2015-03-26 19:27:11 +0100

    Update classes I18nBean and ViewBean

--- src/main/java/views/beans/I18nBean.java
+++ src/main/java/views/beans/I18nBean.java
@@ -1,15 +1,23 @@
 package views.beans;
 
 import java.io.Serializable;
+import java.util.Locale;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 @ManagedBean
-public class I18nBean extends ViewBean implements Serializable{
+public class I18nBean implements Serializable{
 	private static final long serialVersionUID = 1L;
+	private Locale locale;
 	
 	public I18nBean() {
-		super();
+		this.locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
 	}
 
+	public String change(String language) {
+        this.locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+        return null;
+    }
 }
--- src/main/java/views/beans/ViewBean.java
+++ src/main/java/views/beans/ViewBean.java
@@ -1,22 +1,16 @@
 package views.beans;
 
-import java.util.Locale;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.context.FacesContext;
 
 import controllers.ControllerFactory;
 
 @ManagedBean
 public abstract class ViewBean {
-	private Locale locale;
-
     @ManagedProperty(value = "#{controllerFactory}")
     private ControllerFactory controllerFactory;
 
     public ViewBean() {
-    	this.locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
 	}
 
 	public void setControllerFactory(ControllerFactory controllerFactory) {
@@ -27,12 +21,6 @@ public abstract class ViewBean {
     	if(controllerFactory == null)
     		controllerFactory = new ControllerFactory();
         return controllerFactory;
-    }
-    
-    public String change(String language) {
-        this.locale = new Locale(language);
-        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
-        return null;
     }
 
 }
